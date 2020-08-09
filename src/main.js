b072d93210d93768ec611e046845b8e9ce9d494f
Ryan Boldi <Ryan.boldi123@gmail.com>
2020-08-09 15:43:31 +0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,8 +3,22 @@ const WIDTH = 800,
 
 function setup() {
     createCanvas(WIDTH, HEIGHT);
-    background(0);
+    background(230);
+
+    stroke(0);
+    strokeWeight(2);
+    line(10, 10, 10, 790);
+    line(10, 10, 790, 10);
+    line(790, 10, 790, 790);
+    line(10, 790, 790, 790);
+
+    line(10, 50, 790, 50);
+    line(10, 740, 790, 740);
+
+
 }
 
 function draw() {
+
+
 }
